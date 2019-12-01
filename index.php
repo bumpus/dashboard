66455aef48e76b6a8f7f4b5c474190a431372d04
Adam M. Bumpus <adam@bump.us>
2019-12-01 10:42:51 -0600

    Fixed some indentation

--- index.php
+++ index.php
@@ -23,43 +23,64 @@ ob_end_clean();
 error_log("_SESSION contains: " . $contents );
 
 if(!(isset($_SESSION['access_token']) && $_SESSION['access_token'])) {
-error_log("index.php: No access_token in SESSION");
-if (! isset($_GET['code'])) {
+  error_log("index.php: No access_token in SESSION");
+  if (! isset($_GET['code'])) {
     error_log("oauth2callback.php \$_GET['code'] is not set");
     $auth_url = $client->createAuthUrl();
     error_log("Redirecting to $auth_url\n");
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
     return;
-} else {
+  } else {
     $client->authenticate($_GET['code']);
     $_SESSION['access_token'] = $client->getAccessToken();
     $_SESSION['refresh_token'] = $client->getRefreshToken();
+
+    error_log("Saved access_token and refresh token in \$_SESSION");
+    ob_start();
+    var_dump($_SESSION );
+    $contents = ob_get_contents();
+    ob_end_clean();
+    error_log("_SESSION contains: " . $contents );
+
     $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/';
     error_log("Redirecting to $redirect_uri\n");
     header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
     return;
-}
+  }
 } else {
-$client->setAccessToken($_SESSION['access_token']);
-error_log("Had an access_token");
-error_log("Checking if access_token expired");
-if($client->isAccessTokenExpired()){
+  $client->setAccessToken($_SESSION['access_token']);
+  error_log("Had an access_token");
+  error_log("Checking if access_token expired");
+  if($client->isAccessTokenExpired()){
     error_log("access_token expired");
     if (isset($_SESSION['refresh_token']) && $_SESSION['refresh_token'])
     {
-        $client->refreshToken($_SESSION['refresh_token']);
-        $_SESSION['access_token'] = $client->getAccessToken();
+      $client->refreshToken($_SESSION['refresh_token']);
+      $_SESSION['access_token'] = $client->getAccessToken();
+
+      error_log("Refreshed access_token and saved to  \$_SESSION");
+      ob_start();
+      var_dump($_SESSION );
+      $contents = ob_get_contents();
+      ob_end_clean();
+      error_log("_SESSION contains: " . $contents );
     }
     else
     {
-        http_response_code(401); // Don't have a refresh code set unauthroized return code
-        $_SERVER['access_token'] = NULL; //NULL out expired access_token
-        return;
+      error_log("No refresh token. responding with 401 status");
+      http_response_code(401); // Don't have a refresh code set unauthroized return code
+      $_SERVER['access_token'] = NULL; //NULL out expired access_token
+      error_log("Removed access_token from   \$_SESSION");
+      ob_start();
+      var_dump($_SESSION );
+      $contents = ob_get_contents();
+      ob_end_clean();
+      error_log("_SESSION contains: " . $contents );
+      return;
     }
 
+  }
 }
-}
-
 
 switch ($file) {
 case '' :
