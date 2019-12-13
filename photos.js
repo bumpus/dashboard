b8e69c3c319f002b7e989177630b71a4f650a604
Adam M. Bumpus <adam@bump.us>
2019-12-13 07:22:15 -0600

    Updated photos.js to get album data from the web service. Removed static list of pictures and the image files from the site.

--- photos.js
+++ photos.js
@@ -1,27 +1,27 @@
-var photoList = [
-  {"file": "IMG_20191019_082126988.jpg", "id": "pic01"},
-  {"file": "IMG_20191021_191908189.jpg", "id": "pic02"},
-  {"file": "IMG_8808.jpg", "id": "pic03"},
-  {"file": "IMG_9180.jpg", "id": "pic04"},
-  {"file": "IMG_9534.jpg", "id": "pic05"},
-  {"file": "IMG_9717.jpg", "id": "pic06"},
-  {"file": "IMG_9750.jpg", "id": "pic07"},
-  {"file": "IMG_9856.jpg", "id": "pic08"},
-  {"file": "IMG_9858.jpg", "id": "pic09"},
-  {"file": "IMG_9883.jpg", "id": "pic10"}];
+var photoList;
 var imageIndex = 0;
 var photoDelay = 30000;
 var previousPhoto;
 var currentPhoto;
 
 function loadPhoto(){
+  console.log("Loading Photo Album Data!");
+  $.getJSON("photos.php", "", startList).fail(function() {console.log("Failed to load photo album. Return to Login Page!"); location.href = '/config.php'});  
+}
+
+function startList(data){
+  //copy the received JSON data into the photoList array
+  photoList = [];
+  $.each(data, function (index, value) {
+    photoList.push({url:value['url'] + "=w640-h640", id:value['id']});
+  });
   currentPhoto = photoList[imageIndex];
-  $(".photos").append("<img style='display: none' id='"+currentPhoto.id+"' src='"+currentPhoto.file+"'></img>");
+  $(".photos").append("<img style='display: none' id='"+currentPhoto.id+"' src='"+currentPhoto.url+"'></img>");
   reveal();
 }
 
 function nextPhoto(){
-  $(".photos").append("<img style='display: none' id='"+currentPhoto.id+"' src='"+currentPhoto.file+"'></img>");
+  $(".photos").append("<img style='display: none' id='"+currentPhoto.id+"' src='"+currentPhoto.url+"'></img>");
   $("#"+previousPhoto.id).fadeOut("slow", reveal);
 }
 
@@ -31,10 +31,12 @@ function reveal(){
   }
   $("#"+currentPhoto.id).fadeIn("slow");
   imageIndex++;
-  if (imageIndex == photoList.length){
-    imageIndex = 0;
-  }
   previousPhoto = currentPhoto;
-  currentPhoto = photoList[imageIndex];
-  setTimeout(nextPhoto, photoDelay);
+  if (imageIndex != photoList.length){
+    currentPhoto = photoList[imageIndex];
+    setTimeout(nextPhoto, photoDelay);
+  }else{
+    imageIndex = 0;
+    setTimeout(loadPhoto, photoDelay);
+  }
 }
