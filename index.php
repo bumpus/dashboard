af83254b82df3b05b160eb1c7607dcdd4b81f69a
Adam Bumpus <adam@bump.us>
2020-03-15 21:20:23 -0500

    Update index.php
    Updated to allow persistent login. Now stores the google access_token and refresh_token in the user's section of the database instead of in the session variable.

--- index.php
+++ index.php
@@ -33,15 +33,76 @@ $client->addScope([
 ]);
 $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
 
-if(!(isset($_SESSION['access_token']) && $_SESSION['access_token']) || ($file == '/oauth2callback.php')) {
+// Determine if we've logged in before
+// 1) Is the user id set in $_SESSION
+// 2) If not, is it set in the login cookie?
+// 3) If 1 or 2, then check if an access_token is stored
+// 4) If access_token is not stored, see if we're going to oauth2callback.php to set it
+// 5) Otherwise, send us off to the login page.
+
+/* Begin be determining if we already know this user */
+$user_email = NULL;
+/* first check in the session data */
+if (isset($_SESSION['user_email'])){
+  $user_email = $_SESSION['user_email'];
+}else if (isset($_COOKIE['login_id']){
+  /* if that didn't work, check for a login_id cookie to lead us to it */
+  $docRef = $firestore->collection('login_ids')->document($_COOKIE['login_id']);
+  $snapshot = $docRef->snapshot();
+  $data = $snapshot->data();
+  if(array_key_exists('user_email', $data)){
+    $user_email = $data['user_email'];
+  }
+}
+
+/* If we have an e-mail address already, then see if Google login data is stored for it */
+$access_token = NULL;
+$refresh_token = NULL;
+$docRef = $firestore->collection('settings')->document($user_email);
+$snapshot = $docRef->snapshot();
+if($snapshot->exists()){
+  $data = $snapshot->data();
+  if(array_key_exists('access_token', $data){
+    $access_token = $data['access_token'];
+  }
+  if(array_key_exists('refresh_token', $data){
+    $refresh_token = $data['refresh_token'];
+  }
+}
+
+
+
+if((NULL != $access_token) || ($file == '/oauth2callback.php')) {
   if (! isset($_GET['code'])) {
     $auth_url = $client->createAuthUrl();
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
     return;
   } else {
     $client->authenticate($_GET['code']);
-    $_SESSION['access_token'] = $client->getAccessToken();
-    $_SESSION['refresh_token'] = $client->getRefreshToken();
+    # get the id of the logged in user and retreive their configuration:
+    $tokeninfo = $client->verifyIdToken();
+    $user_email = $tokeninfo['email'];
+    $_SESSION['user_email'] = $user_email;
+
+    /* Store the user's e-mail address in the login_id database
+     * If the user already has a login_id cookie set, refresh it.
+     * Otherwise, generate a new one, set cookie and store in database */
+
+    if(isset($_COOKIE['user_id'])){
+      // Get the user_id random string supplied in the user's cookie
+      $user_id = $_COOKIE['user_id'];
+    }else{
+      // Generate a new random user_id login id for the cookie
+      $user_id = bin2hex(random_bytes(16));
+    }
+    $docRef = $firestore->collection('login_ids')->document($user_id);
+    $docRef->set(['user_email' => $user_email], ['merge' => false]);
+
+    // Get the user's database entry and store the access_token and refresh_token
+    $docRef = $firestore->collection('settings')->document($user_email);
+    $docRef->set(['access_token' => $client->getAccessToken(), 'refresh_token' => $client->getRefreshToken()], ['merge' => true]);
+
+    setcookie('user_id', $user_id, time()+(86400 * 30), '/', $_SERVER['SERVER_NAME'], true, true);
 
     $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/';
     header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
@@ -52,15 +113,36 @@ if(!(isset($_SESSION['access_token']) && $_SESSION['access_token']) || ($file ==
   if($client->isAccessTokenExpired()){
     if (isset($_SESSION['refresh_token']) && $_SESSION['refresh_token'])
     {
-      $client->refreshToken($_SESSION['refresh_token']);
-      $_SESSION['access_token'] = $client->getAccessToken();
+      $docRef = $firestore->collection('settings')->document($user_email);
+      $snapshot = $docRef->snapshot();
+      $client->refreshToken($snapshot->data()['refresh_token']);
+      // Get the user's database entry and store the access_token and refresh_token
+      $docRef->set(['access_token' => $client->getAccessToken(), 'refresh_token' => $client->getRefreshToken()], ['merge' => true]);
+
+      /* Store the user's e-mail address in the login_id database
+       * If the user already has a login_id cookie set, refresh it.
+       * Otherwise, generate a new one, set cookie and store in database */
+
+      if(isset($_COOKIE['user_id'])){
+        // Get the user_id random string supplied in the user's cookie
+        $user_id = $_COOKIE['user_id'];
+    }else{
+      // Generate a new random user_id login id for the cookie
+      $user_id = bin2hex(random_bytes(16));
+    }
+    $docRef = $firestore->collection('login_ids')->document($user_id);
+    $docRef->set(['user_email' => $user_email], ['merge' => false]);
+
+    setcookie('user_id', $user_id, time()+(86400 * 30), '/', $_SERVER['SERVER_NAME'], true, true);
+
     }
     else
     {
-      error_log("No refresh token. responding with 401 status");
+      error_log("No refresh token. responding with 403 status");
       http_response_code(403); // Don't have a refresh code set unauthroized return code
-      $_SERVER['access_token'] = NULL; //NULL out expired access_token
-      error_log("Removed access_token from   \$_SESSION");
+      $docRef = $firestore->collection('settings')->document($user_email);
+      $docRef->set(['access_token' => NULL], ['merge' => true]); //NULL out expired access_token
+      error_log("Removed access_token for $user_email");
       return;
     }
 
