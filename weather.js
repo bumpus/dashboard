4b3d415bf21c5e21e1432406cca9dedfeb2b8a9c
Adam M. Bumpus <adam@bump.us>
2020-03-22 10:56:23 -0500

    Fixed a bug in the last couple days before a new moon where rounding didn't get the right index into the moon phase array.

--- weather.js
+++ weather.js
@@ -71,7 +71,7 @@ function displayWeather(data){
   $("#almanac > .dayContainer").append(`<div class="sunrise">${sunriseIcon}${data.almanac.sunrise}</div>`);
   $("#almanac > .dayContainer").append(`<div class="sunset">${sunsetIcon}${data.almanac.sunset}</div>`);
   // Math here converts from decimal moonphase 0=new 0.5 = full to one of the 8 icons in the array above
-  $("#almanac > .dayContainer").append(`<div class="moonphase">${moonIcon[Math.round((data.almanac.moonphase*8)%8)]}</div>`);
+  $("#almanac > .dayContainer").append(`<div class="moonphase">${moonIcon[Math.round(data.almanac.moonphase*8)%8]}</div>`);
 
   //Make the animated icons start animating
   currentskycons.play();
