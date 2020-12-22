ff8bf2a38d25171f1cfcf9c360da07c816fc0f15
Adam M. Bumpus <adam@bump.us>
2020-12-22 17:29:15 +0000

    Removed the skycons.js script from the main html. File was removed earlier and we were getting 404 errors.

--- main.php
+++ main.php
@@ -3,7 +3,6 @@
   <head>
     <title>Bump.us Dashboard</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="skycons.js" defer></script>
     <script src="weather.js" defer></script>
     <script src="menu.js" defer></script>
     <script src="photos.js" defer></script>
